369c8573e7a8d9f8491733485172df3b3ed7cccf
avendesta <53615807+avendesta@users.noreply.github.com>
2021-01-29 02:03:45 +0300

    Added dummy links and templates, inherited from base template, added bootstrap, next needs static css and js

--- shortner_project/shortner/views.py
+++ shortner_project/shortner/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+links = [
+    {
+        'id': '1',
+        'title': 'google class room',
+        'long': 'https://www.google.com/google-classroom',
+        'short': 'short.et/4J3',
+        'created': 'June 23, 2020'
+    },
+    {
+        'id': '2',
+        'title': 'ebay watch',
+        'long': 'https://www.ebay.com/b/Wristwatches/31387/bn_2408451',
+        'short': 'short.et/4G6',
+        'created': 'June 25, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<a href="https://google.com">Go to google</a>')
+    context = {
+        'links': links
+    }
+    return render(request, 'shortner/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'shortner/about.html', {'title':'About'})
