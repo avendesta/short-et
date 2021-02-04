88fc4b4fb379ade11291718fe4375d9e796015b1
avendesta <53615807+avendesta@users.noreply.github.com>
2021-02-04 12:13:04 +0300

    Added model for links, and created few sample links, added links to admin page gui

--- shortner_project/shortner/models.py
+++ shortner_project/shortner/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Link(models.Model):
+    title = models.CharField(max_length=100)
+    short_url = models.URLField()
+    long_url = models.URLField()
+    date_created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    click_count = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}, {self.short_url}"
--- shortner_project/shortner/views.py
+++ shortner_project/shortner/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
+from .models import Link
 
-links = [
-    {
-        'id': '1',
-        'title': 'google class room',
-        'long': 'https://www.google.com/google-classroom',
-        'short': 'short.et/4J3',
-        'created': 'June 23, 2020'
-    },
-    {
-        'id': '2',
-        'title': 'ebay watch',
-        'long': 'https://www.ebay.com/b/Wristwatches/31387/bn_2408451',
-        'short': 'short.et/4G6',
-        'created': 'June 25, 2020'
-    }
-]
 
 def home(request):
     context = {
-        'links': links
+        'links': Link.objects.all()
     }
     return render(request, 'shortner/home.html', context)
 
